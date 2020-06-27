fb149a69a24e46de7aa1c5383f0146692eed1b4b
Beckett Hanan <beckett.hanan@gmail.com>
2020-06-27 15:03:47 -0700

    Changed Footer styling and add links to the two pages we want to link from the footer

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -3,5 +3,13 @@ import React from 'react';
 import styles from './Footer.css';
 
 export default (props) => {
-  return <div className={styles.Footer}></div>;
+  return (
+    <div className={styles.Footer}>
+      <span>
+        <a href="/note">Note To Our Users</a>
+        <span> | </span>
+        <a href="/about">About Us</a>
+      </span>
+    </div>
+  );
 };
