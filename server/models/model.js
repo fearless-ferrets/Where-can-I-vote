bb471bfacd20dd5d8fdb600c77a262f8288c0057
Nadya Minkova <nadya@broko.bg>
2020-06-30 04:37:24 +0300

    logins and signups

--- server/models/model.js
+++ server/models/model.js
@@ -1,9 +1,15 @@
 const { Pool } = require('pg');
 
 const DB_URI = process.env.POSTGRES_URI;
+// console.log(DB_URI);
 
 const dbconnect = new Pool({
   connectionString: DB_URI,
 });
 
-module.exports = dbconnect;
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query : ', text);
+    return dbconnect.query(text, params, callback);
+  },
+};
