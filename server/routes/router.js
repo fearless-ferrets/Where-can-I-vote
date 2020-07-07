c279d2a81a6ffff9019e505d6dbe494adecdb292
Beckett Hanan <beckett.hanan@gmail.com>
2020-07-06 20:09:09 -0700

    removing require statement for user controller in our main router route since we're not using it in there

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const controller = require('../controllers/api-controller.js');
-const userController = require('../controllers/user-controllers');
 
 const router = express.Router();
 
@@ -9,7 +8,6 @@ router.get(
   controller.apiQueries,
   (req, res) => {
     console.log('saving final election data object for front end');
-    // console.log(res.locals.electionData);
     res.status(200).json(res.locals.electionData);
   }
 );
