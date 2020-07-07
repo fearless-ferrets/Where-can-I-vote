9eec48d97e4af727eb6bbff71948efdc6482bdbd
Beckett Hanan <beckett.hanan@gmail.com>
2020-07-06 20:10:14 -0700

    editing NavigationBar component so that when logged in we see Profile and Log Out and when logged out we see Log In and Sign Up in the menu

--- client/components/NavigationBar/NavigationBar.jsx
+++ client/components/NavigationBar/NavigationBar.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logOut } from '../../actions/user';
 import styles from './Navigation.css';
 
-export default (props) => {
+const NavBar = ({ loggedIn, logOut }) => {
   return (
     <div className={styles.NavBar}>
       <div className={styles.site__title}>
         <Link to="/">Where Can I Vote?</Link>
       </div>
       <div className={styles.login__signup}>
-        <span>
-          <Link to="/log-in">Log In</Link>
-          <span> | </span>
-          <Link to="/sign-up">Sign Up</Link>
-        </span>
+        {!loggedIn && (
+          <span>
+            <Link to="/log-in">Log In</Link>
+            <span> | </span>
+            <Link to="/sign-up">Sign Up</Link>
+          </span>
+        )}
+        {loggedIn && (
+          <span>
+            <Link to="/profile">Profile</Link>
+            <span> | </span>
+            <Link to="/" onClick={logOut}>Log Out</Link>
+          </span>
+        )}
       </div>
     </div>
   );
 };
+
+const mapStateToProps = (state) => ({
+  loggedIn: state.userReducer.loggedIn,
+});
+
+const mapDispatchToProps = { logOut };
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
